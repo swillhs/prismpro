229f140e53f0ec62ef0e9a3fb3c76fb931b73e3e
Sarah Hernandez <sarah.hernandez@nutanix.com>
2020-10-28 11:52:16 -0700

    update delete entities script

--- prismpro/lab/removePrismProCluster.sh
+++ prismpro/lab/removePrismProCluster.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-python deleteprismprocluster.py
+python deleteprismproentities.py
 
 cd ~/bin
 python ./unregistration_cleanup.py 00057d50-00df-b390-0000-00000000eafd
+
+zkrm /appliance/physical/clusterexternalstate/00057d50-00df-b390-0000-00000000eafd
+zkrm /appliance/physical/clusterdatastate/00057d50-00df-b390-0000-00000000eafd
+zkrm /appliance/physical/zeusconfig/00057d50-00df-b390-0000-00000000eafd
